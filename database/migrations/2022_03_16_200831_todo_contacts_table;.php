71d40ddc9d98a9c144c698d3fd1d2d4d85cc1a91
lutogb27 <lutogb27@yahoo.co.jp>
2022-03-16 20:53:38 +0900

    修正8

--- database/migrations/2022_03_16_200831_todo_contacts_table;.php
+++ database/migrations/2022_03_16_200831_todo_contacts_table;.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateContactsTable extends Migration
+class TodoContactsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateContactsTable extends Migration
      */
     public function up()
     {
-        Schema::create('Todos', function (Blueprint $table) {
+        Todo::create('Todos', function (Blueprint $table) {
             $table->id();
             $table->string('Todos',191);
             $table->timestamps();
