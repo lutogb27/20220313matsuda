cdf531488cfc70e023aafe6e376243fbea0da9c8
lutogb27 <lutogb27@yahoo.co.jp>
2022-03-20 16:09:39 +0900

    修正12

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,9 @@ use App\Http\Controllers\TodoController;
 */
 
 Route::get('/', [TodoController::class, 'index']);
-Route::post('/todo/create', 'TodoController@create')->name('todo.create');
-Route::post('/todo/create', [TodoController::class, 'create']);
-Route::post('/todo/update', 'TodoController@update')->name('todo.update');
-Route::post('/todo/update', [TodoController::class, 'update']);
-Route::post('/todo/delete', [TodoController::class, 'delete']);
-Route::post('/todo/delete', 'TodoController@update')->name('todo.delete');
+Route::post('/todo/create',  [TodoController::class, 'create'])->name('todo.create');
+//Route::post('/todo/create', [TodoController::class, 'create']);
+Route::post('/todo/update', [TodoController::class, 'update'])->name('todo.update');
+//Route::post('/todo/update', [TodoController::class, 'update']);
+//Route::post('/todo/delete', [TodoController::class, 'delete']);
+Route::post('/todo/delete', [TodoController::class, 'delete'])->name('todo.delete');
